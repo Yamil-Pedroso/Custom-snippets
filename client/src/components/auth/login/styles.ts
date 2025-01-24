02188501bc1b1f62ae5a7ffa65cf885a6b08492c
Yami Pedroso-AI <yamirovinci@gmail.com>
2025-01-24 12:11:12 +0100

    Added some snippets

--- client/src/components/auth/login/styles.ts
+++ client/src/components/auth/login/styles.ts
@@ -49,7 +49,7 @@ export const Button = styled.button`
   padding: 10px 20px;
   font-size: 16px;
   color: #fff;
-  background-color: #007bff;
+  background-color: #4b6929;
   border: none;
   border-radius: 4px;
   cursor: pointer;
@@ -57,7 +57,7 @@ export const Button = styled.button`
   transition: background-color 0.3s;
 
   &:hover {
-    background-color: #0056b3;
+    background-color: #32471b;
   }
 `;
 
