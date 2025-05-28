7e2ae9ce3e94a3a594c4f3a725281cdc62a82abf
Yamil Pedroso <yamirovinci@gmail.com>
2025-05-28 13:46:39 +0200

    Changed to Render backend deploy

--- client/src/api/axiosConfig.ts
+++ client/src/api/axiosConfig.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
-//axios.defaults.baseURL = "https://custom-snippets.onrender.com/api/v1";
-axios.defaults.baseURL = "http://localhost:3010/api/v1";
+axios.defaults.baseURL = "https://custom-snippets.onrender.com/api/v1";
+//axios.defaults.baseURL = "http://localhost:3010/api/v1";
 
 // Interceptor para incluir el token en cada solicitud
 axios.interceptors.request.use((config) => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("authToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axios;
